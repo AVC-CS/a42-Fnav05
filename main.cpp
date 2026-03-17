215831ec125964de1adf7340b7ad1aab3de5b588
Fnav05 <felvinnavarro10@gmail.com>
2026-03-16 20:12:10 -0700

    commit test

--- main.cpp
+++ main.cpp
@@ -28,16 +28,40 @@ int main()
     const double RATE20 = 4.80;
     double weight, distance, dRate, price;
 
-    cout << "Enter the package weight and distance\n";
     cin >> weight >> distance;
 
     // TODO: validate input - weight must be > 0 and <= 20
+    if (weight <= 0 || weight > 20) {
+        cout << "Invalid weight. Must be between 0 and 20" << endl;
+        return 0;
+    }
 
     // TODO: validate input - distance must be >= 10 and <= 3000
+    if (distance < 10 || distance > 3000) {
+        cout << "Invalid distance. Must be between 10 and 3000" << endl;
+        return 0;
+    }
 
     // TODO: determine rate based on weight
+    if (weight <= 2) {
+        dRate = RATE2;
+    }
+    else if (weight <= 6) {
+        dRate = RATE6;
+    }
+    else if (weight <= 10) {
+        dRate = RATE10;
+    }
+    else
+        dRate = RATE20;
 
     // TODO: calculate price based on distance
+    if (distance > 500) {
+        price = (distance / 500) * dRate;
+    }
+    else {
+        price = dRate;
+    }
 
     // Use the following statements to print output
     cout << setprecision(2) << fixed;
